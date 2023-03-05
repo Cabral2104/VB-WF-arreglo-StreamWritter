026fda6693414c184ebc23c062d23977adf012c5
Carlos Emilio Cabral <Carlos Emilio Cabral@DESKTOP-UBTP0PR>
2023-03-05 12:00:29 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.lblContactos = New System.Windows.Forms.Label()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.lblTelefono = New System.Windows.Forms.Label()
+        Me.lblCorreo = New System.Windows.Forms.Label()
+        Me.lblFechaNacimiento = New System.Windows.Forms.Label()
+        Me.txtNumContactos = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.txtCorreo = New System.Windows.Forms.TextBox()
+        Me.dtpFechaNacimiento = New System.Windows.Forms.DateTimePicker()
+        Me.lblGuardados = New System.Windows.Forms.Label()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.SuspendLayout()
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(513, 42)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(171, 86)
+        Me.btnGuardar.TabIndex = 0
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'lblContactos
+        '
+        Me.lblContactos.AutoSize = True
+        Me.lblContactos.Location = New System.Drawing.Point(126, 9)
+        Me.lblContactos.Name = "lblContactos"
+        Me.lblContactos.Size = New System.Drawing.Size(94, 13)
+        Me.lblContactos.TabIndex = 1
+        Me.lblContactos.Text = "Num de contactos"
+        '
+        'lblNombre
+        '
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.Location = New System.Drawing.Point(126, 42)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(44, 13)
+        Me.lblNombre.TabIndex = 2
+        Me.lblNombre.Text = "Nombre"
+        '
+        'lblTelefono
+        '
+        Me.lblTelefono.AutoSize = True
+        Me.lblTelefono.Location = New System.Drawing.Point(126, 76)
+        Me.lblTelefono.Name = "lblTelefono"
+        Me.lblTelefono.Size = New System.Drawing.Size(49, 13)
+        Me.lblTelefono.TabIndex = 3
+        Me.lblTelefono.Text = "Telefono"
+        '
+        'lblCorreo
+        '
+        Me.lblCorreo.AutoSize = True
+        Me.lblCorreo.Location = New System.Drawing.Point(126, 111)
+        Me.lblCorreo.Name = "lblCorreo"
+        Me.lblCorreo.Size = New System.Drawing.Size(94, 13)
+        Me.lblCorreo.TabIndex = 4
+        Me.lblCorreo.Text = "Correo Eléctronico"
+        '
+        'lblFechaNacimiento
+        '
+        Me.lblFechaNacimiento.AutoSize = True
+        Me.lblFechaNacimiento.Location = New System.Drawing.Point(126, 145)
+        Me.lblFechaNacimiento.Name = "lblFechaNacimiento"
+        Me.lblFechaNacimiento.Size = New System.Drawing.Size(108, 13)
+        Me.lblFechaNacimiento.TabIndex = 5
+        Me.lblFechaNacimiento.Text = "Fecha de Nacimiento"
+        '
+        'txtNumContactos
+        '
+        Me.txtNumContactos.Location = New System.Drawing.Point(254, 2)
+        Me.txtNumContactos.Name = "txtNumContactos"
+        Me.txtNumContactos.Size = New System.Drawing.Size(64, 20)
+        Me.txtNumContactos.TabIndex = 6
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(254, 35)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(184, 20)
+        Me.txtNombre.TabIndex = 7
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(254, 73)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(132, 20)
+        Me.txtTelefono.TabIndex = 8
+        '
+        'txtCorreo
+        '
+        Me.txtCorreo.Location = New System.Drawing.Point(254, 108)
+        Me.txtCorreo.Name = "txtCorreo"
+        Me.txtCorreo.Size = New System.Drawing.Size(196, 20)
+        Me.txtCorreo.TabIndex = 9
+        '
+        'dtpFechaNacimiento
+        '
+        Me.dtpFechaNacimiento.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpFechaNacimiento.Location = New System.Drawing.Point(254, 139)
+        Me.dtpFechaNacimiento.Name = "dtpFechaNacimiento"
+        Me.dtpFechaNacimiento.Size = New System.Drawing.Size(105, 20)
+        Me.dtpFechaNacimiento.TabIndex = 10
+        '
+        'lblGuardados
+        '
+        Me.lblGuardados.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.lblGuardados.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.lblGuardados.Location = New System.Drawing.Point(95, 221)
+        Me.lblGuardados.Name = "lblGuardados"
+        Me.lblGuardados.Size = New System.Drawing.Size(672, 329)
+        Me.lblGuardados.TabIndex = 11
+        '
+        'ListView1
+        '
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(712, 121)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(121, 97)
+        Me.ListView1.TabIndex = 12
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(947, 559)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.lblGuardados)
+        Me.Controls.Add(Me.dtpFechaNacimiento)
+        Me.Controls.Add(Me.txtCorreo)
+        Me.Controls.Add(Me.txtTelefono)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.txtNumContactos)
+        Me.Controls.Add(Me.lblFechaNacimiento)
+        Me.Controls.Add(Me.lblCorreo)
+        Me.Controls.Add(Me.lblTelefono)
+        Me.Controls.Add(Me.lblNombre)
+        Me.Controls.Add(Me.lblContactos)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents lblContactos As Label
+    Friend WithEvents lblNombre As Label
+    Friend WithEvents lblTelefono As Label
+    Friend WithEvents lblCorreo As Label
+    Friend WithEvents lblFechaNacimiento As Label
+    Friend WithEvents txtNumContactos As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents txtCorreo As TextBox
+    Friend WithEvents dtpFechaNacimiento As DateTimePicker
+    Friend WithEvents lblGuardados As Label
+    Friend WithEvents ListView1 As ListView
+End Class
---
+++ Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+    Private length As Integer = 0
+    Private index As Integer = 0
+    Private valor As Integer
+    Private array As Contacto()
+
+    Private nombreArchivo As String ' Agregamos una variable para guardar el nombre del archivo
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Generamos el nombre del archivo y lo guardamos en la variable correspondiente
+        nombreArchivo = My.Computer.FileSystem.SpecialDirectories.Desktop & "\contactos.txt"
+        ' Si el archivo no existe, lo creamos con un encabezado
+        If Not My.Computer.FileSystem.FileExists(nombreArchivo) Then
+            My.Computer.FileSystem.WriteAllText(nombreArchivo, "Lista de contactos:" & vbCrLf & vbCrLf, False)
+        End If
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        valor = Integer.Parse(txtNumContactos.Text)
+        index = valor
+        array = New Contacto(index - 1) {}
+        If length < index Then
+            Dim x = New Contacto()
+            x.nombre_ = txtNombre.Text
+            x.fechadenacimiento_ = dtpFechaNacimiento.Value
+            x.telefono_ = txtTelefono.Text
+            x.correo_ = txtCorreo.Text
+
+            ' Crear la cadena de texto con los datos del contacto
+            Dim datosContacto As String = "Nombre: " & x.nombre_ & vbCrLf & "Fecha de nacimiento: " & x.fechadenacimiento_.ToString() & vbCrLf & "Teléfono: " & x.telefono_ & vbCrLf & "Correo electrónico: " & x.correo_ & vbCrLf & vbCrLf
+
+            ' Agregar la cadena al final del archivo de texto
+            My.Computer.FileSystem.WriteAllText(nombreArchivo, datosContacto, True)
+
+            array(length) = x
+            length += 1
+            Dim linea As String = x.ToString() + Environment.NewLine
+            lblGuardados.Text = lblGuardados.Text & linea
+            txtNombre.Clear()
+            dtpFechaNacimiento.Value = DateTime.Now
+            txtTelefono.Clear()
+            txtCorreo.Clear()
+        End If
+
+        If length = index Then
+            MessageBox.Show("se han registrado todos")
+        End If
+    End Sub
+
+    Private Sub txtNcontactos_TextChanged(ByVal sender As Object, ByVal e As EventArgs)
+
+    End Sub
+
+    Private Sub txtNombre_TextChanged(sender As Object, e As EventArgs) Handles txtNombre.TextChanged
+
+    End Sub
+End Class
